a42cf5e6ab136b468121a369255dd00e8fe9f948
jingu-lee <jingu@dable.io>
2022-04-14 18:03:49 +0900

    initial commit

---
+++ utils/slack.py
@@ -0,0 +1,32 @@
+from slackboy import SlackBoy
+
+class SlackReporter:
+    def __init__(self, token: str, channel: str, prefix: str, repository: str,
+                 branch: str, target: str, pr_number: str, tag: str):
+        self.client = SlackBoy.from_token(token)
+        self.client.add_default_channel(channel)
+        self.prefix = prefix
+        self.repository = repository
+        self.branch = branch
+        self.target = target
+        self.pr_number = pr_number
+        self.tag = tag
+
+    def get_pull_request_url(self) -> str:
+        return f'https://github.com/teamdable/{self.repository}/pull/{self.pr_number}'
+
+
+    def get_slack_message(self, msg: str) -> str:
+        return (
+            f'*Repository: {self.repository}* \n\n'
+            '*Pull-request info:*\n'
+            f'- branch: {self.branch} -> {self.target}\n'
+            f'- url: {self.get_pull_request_url}\n\n'
+            f'```{msg}```\n'
+            f'tag: {self.tag}_WARNING'
+        )
+
+
+    def send_slack_message(self, msg: str):
+        message = self.get_slack_message(msg)
+        self.client.send_message(msg=message, prefix=self.prefix)
---
+++ utils/version.py
@@ -0,0 +1,50 @@
+class VersionTarget:
+    def __init__(self, diff_file:str):
+        self.prev_version = None
+        self.next_version = None
+        self.diff_file = diff_file
+    
+    def get_version_string(self):
+        if self.diff_file == '.version.diff':
+            return '__version__'
+        elif self.diff_file == '_version.py.diff':
+            return '__version__'
+        elif self.diff_file == 'package.json.diff':
+            return 'version'
+        else:
+            return 'unknown'
+
+    def check_version_modified(self):
+        with open(self.diff_file, 'r', encoding='utf-8') as f:
+            for line in f.readlines():
+                version_string = self.get_version_string()
+                if line.startswith("-" + version_string):
+                    self.prev_version = line.split('=')[1].replace('"', '').replace("'","").strip()
+                if line.startswith("+" + version_string):
+                    self.next_version = line.split('=')[1].replace('"', '').replace("'","").strip()
+        
+        if self.prev_version is None or self.next_version is None:
+            return False
+        else:
+            return True
+
+    def check_version_update(self):
+        if 'a' in self.prev_version and 'a' in self.next_version:
+            if int(self.prev_version.split('a')[1]) < int(self.next_version.split('a')[1]):
+                return True
+            else:
+                return False
+        elif 'b' in self.prev_version and 'b' in self.next_version:
+            if int(self.prev_version.split('b')[1]) < int(self.next_version.split('b')[1]):
+                return True
+            else:
+                return False
+        elif 'rc' in self.prev_version and 'rc' in self.next_version:
+            if int(self.prev_version.split('rc')[1]) < int(self.next_version.split('rc')[1]):
+                return True
+            else:
+                return False
+        elif self.prev_version < self.next_version:
+            return True
+        else:
+            return False
